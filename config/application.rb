524887bd3895611a73720bc34a0428b2f453377c
TranHuyHoang95 <tranhuyhoangvct@gmail.com>
2021-08-02 17:43:53 +0700

    Refs Task: Chapter 3 - Mostly static pages

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative "boot"
-
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,7 +9,9 @@ module SampleApp1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
