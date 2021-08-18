f5e27d0a29116327981558fe6e37f63e009ea1df
Tran Huy Hoang <tranhuyhoangvct@gmail.com>
2021-08-18 15:17:19 +0700

    Refs Task: Chapter 7 - Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   before_save{email.downcase!}
   validates :name, presence: true, length: {maximum: Settings.length.max_name}
-  validates :email, presence: true, length: {maximum: Settings.length.max_email},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: {case_sensitive: false}
+  validates :email, presence: true, length: {
+    maximum: Settings.length.max_email
+  }, format: {
+    with: VALID_EMAIL_REGEX
+  }, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {
+    minimum: Settings.length.min_password
+  }
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.length.min_password}
 end
