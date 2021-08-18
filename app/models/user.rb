caf59ace0b4bada354ad2ef03735abd1acbe670f
Tran Huy Hoang <tranhuyhoangvct@gmail.com>
2021-08-18 15:17:19 +0700

    Refs Task: Chapter 8-Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
     minimum: Settings.length.min_password
   }
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
