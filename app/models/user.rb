de2818b8262ee30c1da267eaff8b198f3d834a1b
Tran Huy Hoang <tranhuyhoangvct@gmail.com>
2021-08-23 16:08:59 +0700

    Refs Task: Chapter 9 - Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,40 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
   before_save{email.downcase!}
   validates :name, presence: true, length: {maximum: Settings.length.max_name}
   validates :email, presence: true,
     length: {maximum: Settings.length.max_email},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {
-    minimum: Settings.length.min_password
-  }
+  validates :password, presence: true,
+    length: {minimum: Settings.length.min_password}
   has_secure_password
 
   def self.digest string
     cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
+      BCrypt::Engine::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
